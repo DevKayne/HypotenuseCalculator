09203be4010ae70473ddc60247dcde59bf1f2c4b
Kayne <78748546+DevKayne@users.noreply.github.com>
2023-04-24 22:27:23 +0800

    Initial commit

---
+++ [src]/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace HypotenuseCalculator
+{
+    class HypotenuseCalculator
+    {
+        static void Main(string[] args)
+        {
+            // First Two Lines
+            Console.WriteLine("Hypotenuse Calculator");
+            Console.WriteLine("Enter Side A:");
+
+            // Read Side A and Save it to a Variable
+            double SideA = Convert.ToDouble(Console.ReadLine());
+
+            // Side B
+            Console.WriteLine("Enter Side B:");
+            double SideB = Convert.ToDouble(Console.ReadLine());
+
+            // Calculations
+            double answer = Math.Round(Math.Sqrt(SideA * SideA + SideB * SideB));
+
+            // Answer
+            Console.WriteLine("The Hypotenuse is equal to: " + answer);
+
+            // Exit with Key
+            Console.WriteLine("Press ~Any~ Key to Exit.");
+            Console.ReadKey();
+        }
+    }
+}
